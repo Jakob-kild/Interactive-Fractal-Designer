ec9a1d7082befdc2bdaeb3de9266f5dd1e1d2461
Jakob Kildegaard Hansen <jakobkilde@gmail.com>
2025-03-18 13:12:01 +0100

    more iterations

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function drawSierpinski() {
   };
 
   // Choose how many points to plot
-  const iterations = 3;
+  const iterations = 100000;
 
   // Draw each point
   for (let i = 0; i < iterations; i++) {
